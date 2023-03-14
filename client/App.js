cd22a65f04054e37356101ea5097fbaf01346166
Siya Choudhary <67403730+siyachoudhary@users.noreply.github.com>
2023-03-13 23:37:15 -0500

    backend auth complete

---
+++ client/App.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+import { View, Text } from 'react-native'
+import React from 'react'
+import {createNativeStackNavigator} from "@react-navigation/native-stack"
+import { NavigationContainer, NavigationHelpersContext } from '@react-navigation/native';
+//  import StackNavigator from './StackNavigator';
+import WelcomeScreen from './src/screens/WelcomeScreen';
+import SignUp from './src/screens/SignUp';
+import SignIn from './src/screens/SignIn';
+import Home from './src/screens/Home';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+   <NavigationContainer>
+      <Stack.Navigator 
+         screenOptions={{
+             headerShown: false
+         }}>
+            {/* <Stack.Screen name='Welcome Screen' component={WelcomeScreen}/> */}
+            {/* <Stack.Screen name='Sign Up' component={SignUp}/> */}
+            {/* <Stack.Screen name='Sign In' component={SignIn}/> */}
+            <Stack.Screen name='Home' component={Home}/>
+      </Stack.Navigator>
+   </NavigationContainer>
+  )
+}
+
+export default App
