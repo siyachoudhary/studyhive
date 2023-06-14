d2d62c56038ebb7929339a1ee97ee3b9b582e797
Siya Jariwala <siyacj08@gmail.com>
2023-06-14 16:45:00 -0500

    restricted orientation to potrait

--- client/src/screens/BaseUrl.js
+++ client/src/screens/BaseUrl.js
@@ -1,2 +1,2 @@
-export const BaseURL = 'http://192.168.1.7:3000/'
-// export const BaseURL = 'http://localhost:3000/'
+// export const BaseURL = 'http://192.168.1.7:3000/'
+export const BaseURL = 'http://localhost:3000/'
